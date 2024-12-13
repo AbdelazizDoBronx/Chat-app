75eb9e6cca6b871843efad6563184038120fe0ea
AbdelazizDoBronx <abdelzizaitmhnd8@gmail.com>
2024-12-13 17:24:29 +0100

    signin and login pages complited

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import  authMessages  from './routes/message.route.js';
 import dotenv from  'dotenv';
 import { dbConnection } from './lib/db.js';
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 const app = express();
+
 dotenv.config();
 const PORT = process.env.PORT;
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+    origin : 'http://localhost:5173',
+    credentials : true,
+}))
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',authMessages);
 app.listen(PORT,()=>{
